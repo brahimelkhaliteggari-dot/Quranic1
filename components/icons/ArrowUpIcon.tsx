077163467adf9fe3e66706868bf817c0929f5176
brahimelkhaliteggari-dot <brahimelkhaliteggari@gmail.com>
2025-11-12 22:08:25 +0100

    Delete components directory

--- components/icons/ArrowUpIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const ArrowUpIcon: React.FC<{ className?: string }> = ({ className = 'w-6 h-6' }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M7 11l5-5m0 0l5 5m-5-5v12" />
-    </svg>
-);
